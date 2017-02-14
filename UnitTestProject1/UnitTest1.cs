880e257c576a75b6a62155276612abe0010e39b1
ma_x_imus <maxzakharov81@gmail.com>
2017-02-14 20:56:39 +0200

    added test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleProject;
+
+namespace UnitTestProject1
+{
+    
+    [TestClass]
+    public class UnitTest1
+    {
+        private MyClass TestClass;
+
+        [TestInitialize]
+        public void TestInit()
+        { TestClass = new.MyClass();
+        }
+
+
+        [TestMethod]
+        public void TestNotcontainNull()
+        {
+            // arrange
+            List<double> vector = new.List<double>();
+            vector.Add(1);
+            vector.Add(2);
+            vector.Add(3);
+
+            //act
+            List<double> expectedResult = new.List<double>();
+            expectedResult.Add(2);
+            expectedResult.Add(3);
+            expectedResult.Add(4);
+
+            List<double> realResult = TestClass.increment_vector();
+
+            CollectionAssert.AreEqual
+
+        }
+
+
+        [TestMethod]
+        public void TestTypeDouble()
+        {
+
+        }
+
+
+
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestPromt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleProject;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestPromt
+    {
+        [TestMethod]
+        public void TestPromt()
+
+
+
+            MyClass TestClass
+
+            //arrange
+        string Teststring = "Mike";
+        string ExpectedTeststring = "Hello, Mike";
+
+        //act
+        MyClass testclass = new MyClass();
+
+        string realresult = TestClassAttribute.promt(Teststring);
+        StringAssert.Equals (ExpectedTeststring
+        {
+        }
+    }
+}
